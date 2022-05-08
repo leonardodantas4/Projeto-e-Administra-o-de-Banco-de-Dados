2009810647af676c1ffecd149c0fb343b8925d67
leonardodantas4 <leonardodds@hotmail.com>
2022-05-08 15:45:38 -0300

    Modificacao tarefa01-q04.sql #1

--- tarefas/t01/tarefa01-q04.sql
+++ tarefas/t01/tarefa01-q04.sql
@@ -1,1 +1,5 @@
-SELECT nome, salario, cod_depto FROM funcionario WHERE codigo NOT IN(SELECT cod_gerente FROM departamento);
+UPDATE departamento SET cod_gerente = 5  WHERE codigo = 1;
+
+CREATE VIEW gerentes(codigo, nome, salario, cod_depto) AS SELECT codigo, nome, salario, cod_depto FROM funcionario WHERE codigo IN (SELECT cod_gerente FROM departamento);
+
+SELECT nome, salario, cod_depto FROM funcionario WHERE codigo NOT IN (SELECT codigo FROM gerentes) ORDER BY cod_depto
