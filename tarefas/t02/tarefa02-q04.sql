9e269878b153f106e72420bd3ae4d678822820b3
leonardodantas4 <leonardodds@hotmail.com>
2022-05-23 22:48:46 -0300

    Correçao questao 4 #2

--- tarefas/t02/tarefa02-q04.sql
+++ tarefas/t02/tarefa02-q04.sql
@@ -1,3 +1,3 @@
 CREATE TRIGGER add_cartao_milhas
 AFTER INSERT ON cliente
-FOR EACH ROW INSERT INTO milhas (quantidade, cliente) VALUES (0, NEW.CODIGO);
+FOR EACH ROW INSERT INTO milhas (quantidade, cliente) VALUES (0, NEW.codigo);
